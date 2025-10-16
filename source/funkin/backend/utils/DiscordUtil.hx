2032aa5bea1fbe9d9b42fffe407233d86f0d3e50
aleksa07 <33092759+aleksa07@users.noreply.github.com>
2025-10-16 22:01:20 +0200

    discord update yata yata

--- source/funkin/backend/utils/DiscordUtil.hx
+++ source/funkin/backend/utils/DiscordUtil.hx
@@ -86,7 +86,7 @@ final class DiscordUtil
 		if (config == null)
 			config = {};
 
-		config.logoKey =  "https://media1.tenor.com/m/Q555g-Ez0KYAAAAC/boykisser-kisser-of-boys.gif"; //config.logoKey.getDefault(Flags.DEFAULT_DISCORD_LOGO_KEY);
+		config.logoKey =  "https://media1.tenor.com/m/eysgXImc5UIAAAAd/bunny-massage.gif"; //config.logoKey.getDefault(Flags.DEFAULT_DISCORD_LOGO_KEY);
 		config.logoText = "Crackhosue entertainment"; //config.logoText.getDefault(Flags.DEFAULT_DISCORD_LOGO_TEXT);
 		config.clientID =  "1424864194871693322"; //config.clientID.getDefault(Flags.DEFAULT_DISCORD_CLIENT_ID);
 		
